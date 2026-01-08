7e667509ea0c915b1a28b7a398473fd0d3a38080
Subaru Putra <subaruputra13@gmail.com>
2026-01-08 17:33:08 +0700

    env delete

--- main.go
+++ main.go
@@ -3,17 +3,15 @@ package main
 import (
 	"Chiprek/config"
 	"Chiprek/routes"
-	"log"
 
-	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("failed to load .env")
-	}
+	// err := godotenv.Load(".env")
+	// if err != nil {
+	// 	log.Fatal("failed to load .env")
+	// }
 
 	db := config.InitDB()
 
